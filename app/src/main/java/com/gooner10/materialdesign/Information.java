729282713bfdf3c8b2dc990899b6739353e3b292
Birat Rai <biratrai@hotmail.com>
2019-05-24 12:53:11 -0400

    converting to kotlin and cleaning code

--- app/src/main/java/com/gooner10/materialdesign/Information.java
+++
@@ -1,9 +0,0 @@
-package com.gooner10.materialdesign;
-
-/**
- * Created by Gooner10 on 5/27/15.
- */
-public class Information {
-    int iconId;
-    String title;
-}
---
+++ app/src/main/java/com/gooner10/materialdesign/Information.kt
@@ -0,0 +1,9 @@
+package com.gooner10.materialdesign
+
+/**
+ * Created by Gooner10 on 5/27/15.
+ */
+class Information {
+     var iconId: Int = 0
+     var title: String? = null
+}
